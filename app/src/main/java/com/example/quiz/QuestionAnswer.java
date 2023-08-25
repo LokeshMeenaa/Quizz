a9035293cb0e4cdfe0b7100d6b3c46d8f152bc55
LokeshMeenaa <Lokeshmeena2251893@gmail.com>
2023-08-25 10:26:58 +0530

    Final Commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android ?",
+            "Which one is not the programming language ?",
+            "Who is the owner of TATA Company ?",
+            "Which company owns the Apple ?"
+
+    };
+
+    public static String choices[] [] ={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Python","Notepad","Python"},
+            {"Gautam","Ratan singh","Jef Bezosh","Elon Musk"},
+            {"Google","Apple","Nokia","Samsung"},
+
+    };
+
+    public static String answer[] ={
+            "Google",
+            "Notepad",
+            "Ratan singh",
+            "Apple"
+
+    };
+}
